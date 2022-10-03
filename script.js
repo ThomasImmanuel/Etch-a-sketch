1ed46edea892b18c19bce7bc4ac230a296bae70d
Thomas Immanuel <thomasivw@gmail.com>
2022-10-03 10:27:15 +0200

    Add input elements and function makeNewGrid()

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const container = document.createElement("div");
 container.classList.add("container");
-container.setAttribute('style','width: 960px; height: 500px; display: grid;')
-
 document.body.appendChild(container);
+document.body.setAttribute('style','display: flex; align-items: center; flex-direction: column;')
 
-function makeGrid (num){
-    for (let i = 0; i <= (num * num) -1; i++) {
-        let div = document.createElement("div");
-        div.setAttribute('style','background-color: green; margin: 1px;');
-        container.appendChild(div);
+const input = document.querySelector('#input');
+
+function makeNewGrid() {
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
     }
-    container.setAttribute('style',`width: 960px; height: 500px; display: grid; grid-template-columns: repeat(${num},1fr);`)
-    
+    makeGrid(parseInt(input.value));
 }
 
-makeGrid(40);
+
+function makeGrid (num){
+    if (num > 100) {
+        console.log('Number to high');
+        return;
+    }
+    for (let i = 0; i <= (num * num) -1; i++) {
+        let div = document.createElement("div");
+        div.setAttribute('style','background-color: green; margin: .5px;');
+        container.appendChild(div);
+    }
+    container.setAttribute('style',`width: 960px; height: 500px; display: grid; grid-template-columns: repeat(${num},1fr);`);
+}
+
+
 
 
 
